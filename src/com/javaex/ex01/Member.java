66f5aabd20865b8ab81c5a9c2c7595d92e662f4f
kangsb-010313 <kangsb668@naver.com>
2025-05-01 17:11:48 +0900

    pt05 250501 17:11

--- src/com/javaex/ex01/Member.java
+++ src/com/javaex/ex01/Member.java
@@ -4,10 +4,24 @@ public class Member {
 
 	
 	//필드
+	private String id;
+	private String name;
+	private int point;
+
+	
 	
 	//생성자
+	public Member() {
+		
+	}	
+	public Member(String id, String name, int point) {
+		this.id = id;
+		this.name = name;
+		this.point = point;
+	}	
+	
 	
 	//메소드 gs
 	
-	//메소드 일
+	//메소드 일반
 }
